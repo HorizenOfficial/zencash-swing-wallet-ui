d9438e1e748b224099307a2223d1097887d62664
Ivan <ivan@vaklinov.com>
2017-07-03 22:54:51 +0300

    Download err. message correction.

--- src/java/com/vaklinov/zcashui/ProvingKeyFetcher.java
+++ src/java/com/vaklinov/zcashui/ProvingKeyFetcher.java
@@ -36,7 +36,7 @@ public class ProvingKeyFetcher {
         try {
             verifyOrFetch(parent);
         } catch (InterruptedIOException iox) {
-            JOptionPane.showMessageDialog(parent, "HUSH cannot proceed without a proving key.");
+            JOptionPane.showMessageDialog(parent, "The ZENCash wallet cannot proceed without a proving key.");
             System.exit(-3);
         }
     }
