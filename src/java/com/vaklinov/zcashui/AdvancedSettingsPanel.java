d2d350e14c228ab4418352bbfa186e537bd52475
josephnicholas <josephnicholasalcantara@gmail.com>
2018-06-25 15:34:19 +0800

    Refactor class name https://github.com/ZencashOfficial/zencash-swing-wallet-ui/issues/108

--- src/java/com/vaklinov/zcashui/AdvancedSettingsPanel.java
+++ src/java/com/vaklinov/zcashui/AdvancedSettingsPanel.java
@@ -28,11 +28,11 @@ public class AdvancedSettingsPanel extends JPanel {
     private JButton btnLinkCommands = null;
     private LanguageUtil langUtil;
 
-    private ZCashUI parent;
+    private ZenCashUI parent;
     private final ZCashClientCaller clientCaller;
 
 
-    public AdvancedSettingsPanel(ZCashUI parent, ZCashClientCaller clientCaller)
+    public AdvancedSettingsPanel(ZenCashUI parent, ZCashClientCaller clientCaller)
             throws IOException, InterruptedException, URISyntaxException {
         super();
         langUtil = LanguageUtil.instance();
