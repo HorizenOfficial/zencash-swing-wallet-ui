64da19e076a6eb0b6ae95e59cdea7898ab788156
cronicc <cronic@zensystem.io>
2021-03-29 13:19:01 +0000

    Update to zend 2.0.23, JDK 16, fix Windows installer read-only permissions

--- scripts/windows-sign.ps1
+++ scripts/windows-sign.ps1
@@ -2,5 +2,6 @@ $Password = ConvertTo-SecureString -String $Env:WIN_CERT_PASSWORD -AsPlainText -
 
 Import-PfxCertificate -FilePath windows.p12 -CertStoreLocation Cert:\LocalMachine\My -Password $Password
 
+Set-ItemProperty -Path $Env:APPLICATION_PATH -Name IsReadOnly -Value $false
 scripts/signtool.exe sign /debug /v /f windows.p12 /t http://timestamp.digicert.com /du https://github.com/HorizenOfficial/zen /p $Env:WIN_CERT_PASSWORD /n "Zen Blockchain Foundation" $Env:APPLICATION_PATH
 scripts/signtool.exe sign /debug /v /f windows.p12 /as /fd sha256 /tr http://timestamp.digicert.com /td sha256 /du https://github.com/HorizenOfficial/zen /p $Env:WIN_CERT_PASSWORD /n "Zen Blockchain Foundation" $Env:APPLICATION_PATH
