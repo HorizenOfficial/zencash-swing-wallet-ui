ac55100b82ab4db281e7dc0808d0dc9c33272f8a
cronicc <cronic@zensystem.io>
2022-03-28 09:28:17 +0000

    Update to zen v3.1.0, version 1.0.6, add bump version script

---
+++ scripts/bump_version_devtool.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euo pipefail
+
+zend_version_old="3.0.3"
+zend_version_new="3.1.0"
+
+swing_version_old="1.0.6"
+swing_version_new="1.0.6"
+
+# bump zend version
+sed -i "s/${zend_version_old//./\\.}/${zend_version_new//./\\.}/g" .travis.yml
+
+# bump swing version
+grep -lr --exclude-dir=.git --exclude-dir=docs "${swing_version_old//./\\.}" | xargs sed -i "s/${swing_version_old//./\\.}/${swing_version_new//./\\.}/g"
+
+# create release notes
+new_notes="docs/Release_${swing_version_new}.md"
+cp "docs/Release_${swing_version_old}.md" "${new_notes}"
+sed -i "s/${zend_version_old//./\\.}/${zend_version_new//./\\.}/g" "${new_notes}"
+sed -i "s/${swing_version_old//./\\.}/${swing_version_new//./\\.}/g" "${new_notes}"
+sed -i 's/is\ not\ `.*`/is\ not\ `TODO`/g' "${new_notes}"
+sed -i 's/^.*\ \ HorizenDesktopGUIWallet/TODO\ \ HorizenDesktopGUIWallet/g' "${new_notes}"
